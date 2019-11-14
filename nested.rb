06eb9ca4f2b1b468526dd691ff70937833262034
Benjamin Small <ben20262@aim.com>
2019-11-14 20:16:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,6 +60,10 @@ def dennis_ritchies_language
 end
 
 def adding_matz
+	programmer_hash[:yukihiro_matsumoto] = {
+		:known_for => "Ruby",
+		:languages => ["LISP", "C"]
+	}
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -75,10 +79,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {
-	:known_for => "Ruby",
-	:languages => ["LISP", "C"]
-}
 end
 
 def changing_alan
