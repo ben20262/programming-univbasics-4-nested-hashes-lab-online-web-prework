74e0410e692f32e1063d65d1307cd1e6178c6857
Benjamin Small <ben20262@aim.com>
2019-11-14 20:14:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,7 @@ programmer_hash[:yukihiro_matsumoto] = {
 	:known_for => "Ruby",
 	:languages => ["LISP", "C"]
 }
-programmer_hash[:yukihiro_matsumoto].keys
+programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
